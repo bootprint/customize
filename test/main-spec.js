8423400808b05420ae2c5c1ffd69a3ed9d869eea
Nils Knappmeier <npm@knappi.org>
2016-03-02 16:06:41 +0100

    More tests, include 'trace' for better stacktraces

--- test/main-spec.js
+++ test/main-spec.js
@@ -14,8 +14,16 @@
 
 'use strict'
 
+require('trace')
+
 var expect = require('chai').expect
 var customize = require('../')
+
+var minimalEngine = {
+  run: function (obj) {
+    return obj
+  }
+}
 
 var ro1 = customize()
   .registerEngine('test', require('./testEngine.js'))
@@ -30,7 +38,10 @@ var ro1 = customize()
         a: {x: 'x1', y: 'y1'},
         b: {x: 'x1', y: 'y1'}
       },
-      array: ['item1']
+      array: ['item1'],
+      withParent: function (value) {
+        return value + 3
+      }
     // TODO: Tests for promises and functions
     }
   })
@@ -57,20 +68,23 @@ describe('After loading a config', function () {
       }
     })
   })
-  it('object values should exist', function () {
+  it('the object values should exist', function () {
     expect(testResult.test.objects).to.eql({
       a: {x: 'x1', y: 'y1'},
       b: {x: 'x1', y: 'y1'}
     })
   })
-  it('leaf values should exist', function () {
+  it('the leaf values should exist', function () {
     expect(testResult.test.leafs).to.eql({
       a: {x: 'x1', y: 'y1'},
       b: {x: 'x1', y: 'y1'}
     })
   })
-  it('array values should exist', function () {
+  it('the array values should exist', function () {
     expect(testResult.test.array).to.eql(['item1'])
+  })
+  it('the withParent-function should exist', function () {
+    expect(testResult.test.withParent(5)).to.equal(8)
   })
 })
 
@@ -91,7 +105,10 @@ describe('After merging another config', function () {
               y: 'y2'
             }
           },
-          array: ['item2']
+          array: ['item2'],
+          withParent: function (value) {
+            return this.parent(value) + 3
+          }
         }
       })
       .run()
@@ -130,6 +147,9 @@ describe('After merging another config', function () {
   })
   it('array values should exist', function () {
     expect(testResult.test.array).to.eql(['item1', 'item2'])
+  })
+  it('the withParent-function should call the parent', function () {
+    expect(testResult.test.withParent(5)).to.equal(11)
   })
 })
 
@@ -171,5 +191,111 @@ describe('after loading a module', function () {
   })
   it('array values should exist', function () {
     expect(testResult.test.array).to.eql(['item1', 'item2'])
+  })
+  it('the withParent-function should properly call the parent-function', function () {
+    expect(testResult.test.withParent(5)).to.equal(16)
+  })
+})
+
+describe('the "merge"-method', function () {
+  it('should not fail merging an empty object', function () {
+    ro1
+      .merge({
+        test: {}
+      })
+      .run()
+  })
+
+  it('should throw an error if the config data for an engine that is not registered', function () {
+    return expect(function () {
+      return ro1.merge({test2: {}})
+    }).to.throw(Error)
+  })
+
+  it('should throw an error if a config is undefined', function () {
+    return expect(function () {
+      return customize()
+        .registerEngine('test', require('./testEngine.js'))
+        // TODO: Is this really necessary? It could also treat an undefined config as empty object
+        .merge(undefined)
+    }).to.throw(Error)
+  })
+
+  it('should throw an error if the merge-configuration does not match the schema', function () {
+    return expect(customize()
+      .registerEngine('test', require('./testEngine.js'))
+      .merge({
+        test: {
+          files: 123
+        }
+      })
+      .buildConfig()).to.be.rejectedWith(Error, /Error while validating Customize configuration/)
+  })
+})
+
+describe('The "registerEngine"-method', function () {
+  it('should throw errors  if a non-string engine-id is used', function () {
+    return expect(function () {
+      return customize().registerEngine(123, require('./testEngine.js'))
+    }).to.throw(Error, /Engine-id must be a string/)
+  })
+
+  it('should throw errors if a engine-id starts with "_"', function () {
+    return expect(function () {
+      return customize().registerEngine('_test', require('./testEngine.js'))
+    }).to.throw(Error, /Engine-id may not start with an underscore/)
+  })
+
+  it('should throw errors if an engine does not have a run method', function () {
+    return expect(function () {
+      // noinspection JSCheckFunctionSignatures
+      return customize().registerEngine('test', {})
+    }).to.throw(Error, /needs a run method/)
+  })
+
+  it('should throw errors if an engine-id is already used', function () {
+    return expect(function () {
+      return customize()
+        .registerEngine('test', require('./testEngine.js'))
+        .registerEngine('test', require('./testEngine.js'))
+    }).to.throw(Error, /already registered/)
+  })
+
+  it('should use an empty default config if none is provided', function () {
+    return expect(customize().registerEngine('test', minimalEngine).run()).to.eventually.deep.equal({test: {}})
+  })
+})
+
+describe('The "configSchema"-method', function () {
+  it('should return a combined configuration schema for the all engines', function () {
+    return expect(customize().registerEngine('test', require('./testEngine.js')).configSchema()).to.deep.equal({
+      '$schema': 'http://json-schema.org/draft-04/schema#',
+      'type': 'object',
+      'properties': {
+        'test': {
+          type: 'object',
+          properties: {
+            'files': {
+              type: 'string'
+            },
+            'objects': {
+              type: 'object'
+            },
+            'leafs': {
+              type: 'object'
+            },
+            'array': {
+              type: 'array',
+              items: {
+                type: 'string'
+              }
+            },
+            withParent: {
+              type: 'function'
+            }
+          }
+        }
+      }
+    })
   })
 })
